8fb8633ac4c5a7bc65809dc420eb83d5642527c5
Exyeus <1728827724@qq.com>
2025-08-22 20:59:14 +0800

    hw 4 repair

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -2,7 +2,10 @@ package hw4.puzzle;
 
 import edu.princeton.cs.algs4.Queue;
 
-public class Board implements WorldState{
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Board implements WorldState {
 
     private final int[][] tiles;
     private final int size;
@@ -26,7 +29,8 @@ public class Board implements WorldState{
      */
 
     public int tileAt(int i, int j) {
-        if (i < 0 || j < 0
+        if (i < 0
+                || j < 0
                 || i > size - 1
                 || j > size - 1) {
             throw new IllegalArgumentException("Indices must be within the range");
@@ -45,14 +49,14 @@ public class Board implements WorldState{
      * @return The neighbors of the current board
      */
     public Iterable<WorldState> neighbors() {
-        int BLANK = 0;
+        int blank = 0;
         Queue<WorldState> neighbors = new Queue<>();
         int hug = size();
         int bug = -1;
         int zug = -1;
         for (int rug = 0; rug < hug; rug++) {
             for (int tug = 0; tug < hug; tug++) {
-                if (tileAt(rug, tug) == BLANK) {
+                if (tileAt(rug, tug) == blank) {
                     bug = rug;
                     zug = tug;
                 }
@@ -68,11 +72,11 @@ public class Board implements WorldState{
             for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
                 if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
                     ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
-                    ili1li1[l11il][lil1il1] = BLANK;
+                    ili1li1[l11il][lil1il1] = blank;
                     Board neighbor = new Board(ili1li1);
                     neighbors.enqueue(neighbor);
                     ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
-                    ili1li1[bug][zug] = BLANK;
+                    ili1li1[bug][zug] = blank;
                 }
             }
         }
@@ -86,11 +90,17 @@ public class Board implements WorldState{
      */
     public int hamming() {
         int misplace  = 0;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size - 1; i++) {
             for (int j = 0; j < size; j++) {
                 if (indicesToCorresNum(i, j) != tileAt(i, j)) {
                     misplace += 1;
                 }
+            }
+        }
+        // Check the last row later
+        for (int j = 0; j < size - 1; j++) {
+            if (indicesToCorresNum(size - 1, j) != tileAt(size - 1, j)) {
+                misplace += 1;
             }
         }
         return misplace;
@@ -103,17 +113,23 @@ public class Board implements WorldState{
      * @return Manhattan distance as an estimate
      */
     public int manhattan() {
-        // TODO: Be cautious about the 0 at (N - 1, N - 1)
+        // TO DO: Be cautious about the 0 at (N - 1, N - 1)
         int manhattanSum = 0;
         int tempTileValue;
         int deservedRowNumber, deservedColumnNumber;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
+                // Iterate through all the tiles.
+                // For example:
+                // 2 0
+                // 1 3; 2 = tileAt(0, 0)
                 tempTileValue = tileAt(i, j);
-                deservedRowNumber = Math.floorDiv(tempTileValue - 1, 2);
-                deservedColumnNumber = (tempTileValue - 1) % size;
-                manhattanSum += (Math.abs(i - deservedRowNumber)
-                        + Math.abs(j - deservedColumnNumber));
+                if (tempTileValue != 0) {
+                    deservedRowNumber = Math.floorDiv(tempTileValue - 1, 2);
+                    deservedColumnNumber = (tempTileValue - 1) % size;
+                    manhattanSum += (Math.abs(i - deservedRowNumber)
+                            + Math.abs(j - deservedColumnNumber));
+                }
             }
         }
         return manhattanSum;
@@ -131,8 +147,14 @@ public class Board implements WorldState{
      * @param y Another board
      * @return return true, if the two boards have the same tiles.
      */
+    @Override
     public boolean equals(Object y) {
-        return false;
+        if (y == null || getClass() != y.getClass()) {
+            return false;
+        } else {
+            return (this.size == ((Board) y).size
+                    && Arrays.deepEquals(this.tiles, ((Board) y).tiles));
+        }
     }
 
     /** Returns the string representation of the board.
@@ -157,6 +179,19 @@ public class Board implements WorldState{
         }
         return size * i + j + 1;
 
+    }
+    @Override
+    public int hashCode() {
+        /*int result = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                result = result + 1;
+                result = result * tiles[i][j];
+            }
+            result %= 0x7fffffff;
+        }
+        return result;*/
+        return Objects.hash(size, Arrays.deepHashCode(tiles))
     }
 
 }
