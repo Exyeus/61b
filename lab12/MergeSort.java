138fc4f26f235f4f07216f3b5d358091c244dd0f
Exyeus <1728827724@qq.com>
2025-08-25 19:51:52 +0800

    lab 12

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -13,6 +13,7 @@ public class MergeSort {
      */
     private static <Item extends Comparable> Item getMin(
             Queue<Item> q1, Queue<Item> q2) {
+        // To directly get the smallest items from 2 queues, can be used in merge step
         if (q1.isEmpty()) {
             return q2.dequeue();
         } else if (q2.isEmpty()) {
@@ -35,7 +36,13 @@ public class MergeSort {
     private static <Item extends Comparable> Queue<Queue<Item>>
             makeSingleItemQueues(Queue<Item> items) {
         // Your code here!
-        return null;
+        // To break small queues, and enable primary comparison and merge
+        Queue<Queue<Item>> resultQueuesOfSplitting = new Queue<>();
+        for (Item item : items) {
+            Queue<Item> itemQueue = new Queue<Item>();
+            resultQueuesOfSplitting.enqueue(itemQueue);
+        }
+        return resultQueuesOfSplitting;
     }
 
     /**
@@ -47,20 +54,90 @@ public class MergeSort {
      *
      * @param   q1  A Queue in sorted order from least to greatest.
      * @param   q2  A Queue in sorted order from least to greatest.
-     * @return      A Queue containing all of the q1 and q2 in sorted order, from least to
+     * @return      A Queue containing all the q1 and q2 in sorted order, from least to
      *              greatest.
      *
      */
     private static <Item extends Comparable> Queue<Item> mergeSortedQueues(
             Queue<Item> q1, Queue<Item> q2) {
         // Your code here!
-        return null;
+        // Merge sorted queues, getMin must be used.
+        Queue<Item> resultQueueOfMerge = new Queue<>();
+
+        while (!(q1.isEmpty() && q2.isEmpty())) {
+            resultQueueOfMerge.enqueue(getMin(q1, q2));
+        }
+        return resultQueueOfMerge;
     }
 
+    private static <Item extends Comparable> Queue<Item> splitQueue(
+            Queue<Item> singleItemQueues, int combineRange) {
+        if (combineRange <= 0 || combineRange >= singleItemQueues.size()) {
+            throw new IllegalArgumentException();
+        }
+        Queue<Item> headPartQueue = new Queue<>();
+        for (int i = 0; i < combineRange; i++) {
+            headPartQueue.enqueue(singleItemQueues.dequeue());
+        }
+        return headPartQueue;
+    }
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
-        // Your code here!
-        return items;
+        // Maybe only calling functions. Asking mergeSortedQueues, to keep merging 2 parts.
+        /* Now we have methods to:
+          Break Queue to lots of singleItem queues; Not knowing to use this at the beginning
+                or after the splitting work is done.
+          Merge the **sorted** 2 queues to a large queue; Final result must be out of this.
+          Also, what is the method for the basic sorting / exchanging work?
+          If there not being help methods, its should be done with current mergeSort method!
+          Note that, its input is a single Queue, not sorted.
+        */
+        Queue<Item> resultQueueOfSort = new Queue<>();
+        if (items.size() == 1) {
+            return items;
+        } else if (items.size() == 2) {
+
+            Item item1 = items.dequeue();
+            Item item2 = items.dequeue();
+
+            Queue<Item> smallSortedQueue = new Queue<>();
+
+            if (item1.compareTo(item2) <= 0) {
+                smallSortedQueue.enqueue(item1);
+                smallSortedQueue.enqueue(item2);
+            } else {
+                smallSortedQueue.enqueue(item2);
+                smallSortedQueue.enqueue(item1);
+            }
+            return smallSortedQueue;
+        } else {
+            Queue<Item> queuePart1 = splitQueue(items, items.size() / 2);
+            // I think due to the destructive change from the splitQueue, the Queue items
+            // refers to only keeps the tail part.
+            return mergeSortedQueues(mergeSort(queuePart1), mergeSort(items));
+        }
+    }
+
+    /** create a Queue of unsorted objects and print that queue.
+     *  Next, call MergeSort.mergeSort() on that queue,
+     *  and print both the original queue (which should be unchanged)
+     *  and the returned, sorted queue.
+     */
+    public static void main(String[] args) {
+        Queue<String> empires = new Queue<String>();
+        String[] empireList = {"China", "Babylon", "India", "Rome", "Cree", "Indonesia", "Khmer", "England",
+                "Sumeria", "Macedon", "Aztec", "France", "Spain", "Colombia", "Scythia", "Carthage", "Zulu", "Congo",
+                "Korea", "Japan", "Ethiopia"};
+
+        for (String item : empireList) {
+            empires.enqueue(item);
+            System.out.print(item + " ");
+        }
+        System.out.print("\n");
+        Queue<String> sortedEmpires = MergeSort.mergeSort(empires);
+        for (String item : sortedEmpires) {
+            System.out.print(item + " ");
+        }
     }
 }
--- lab12/QuickSort.java
+++ lab12/QuickSort.java
@@ -4,17 +4,17 @@ public class QuickSort {
     /**
      * Returns a new queue that contains the given queues catenated together.
      *
-     * The items in q2 will be catenated after all of the items in q1.
+     * The items in q2 will be concatenated after all the items in q1.
      */
     private static <Item extends Comparable> Queue<Item> catenate(Queue<Item> q1, Queue<Item> q2) {
-        Queue<Item> catenated = new Queue<Item>();
+        Queue<Item> concatenated = new Queue<Item>();
         for (Item item : q1) {
-            catenated.enqueue(item);
+            concatenated.enqueue(item);
         }
         for (Item item: q2) {
-            catenated.enqueue(item);
+            concatenated.enqueue(item);
         }
-        return catenated;
+        return concatenated;
     }
 
     /** Returns a random item from the given queue. */
@@ -38,22 +38,77 @@ public class QuickSort {
      * @param unsorted  A Queue of unsorted items
      * @param pivot     The item to pivot on
      * @param less      An empty Queue. When the function completes, this queue will contain
-     *                  all of the items in unsorted that are less than the given pivot.
+     *                  all the items in unsorted that are less than the given pivot.
      * @param equal     An empty Queue. When the function completes, this queue will contain
-     *                  all of the items in unsorted that are equal to the given pivot.
+     *                  all the items in unsorted that are equal to the given pivot.
      * @param greater   An empty Queue. When the function completes, this queue will contain
-     *                  all of the items in unsorted that are greater than the given pivot.
+     *                  all the items in unsorted that are greater than the given pivot.
      */
     private static <Item extends Comparable> void partition(
             Queue<Item> unsorted, Item pivot,
             Queue<Item> less, Queue<Item> equal, Queue<Item> greater) {
         // Your code here!
+        // Instantiate the empty queues in quickSort method, pass them into this partition method,
+        // and concatenate them after exert quickSort onto them recursively.
+        // Launch the analysis work, from the Queue unsorted.
+        // Item pivot, random or adopt the first directly?
+        for (Item currentItem : unsorted) {
+            if (currentItem.compareTo(pivot) < 0) {
+                less.enqueue(currentItem);
+            } else if (currentItem.compareTo(pivot) == 0) {
+                equal.enqueue(currentItem);
+            } else {
+                greater.enqueue(currentItem);
+            }
+        }
     }
 
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> quickSort(
             Queue<Item> items) {
         // Your code here!
-        return items;
+        if (items.size() <= 1) {
+            return items;
+        } else if (items.size() == 2) {
+            Item item1 = items.dequeue();
+            Item item2 = items.dequeue();
+
+            Queue<Item> smallSortedQueue = new Queue<>();
+
+            if (item1.compareTo(item2) <= 0) {
+                smallSortedQueue.enqueue(item1);
+                smallSortedQueue.enqueue(item2);
+            } else {
+                smallSortedQueue.enqueue(item2);
+                smallSortedQueue.enqueue(item1);
+            }
+            return smallSortedQueue;
+        } else {
+            Item pivot = getRandomItem(items);
+            Queue<Item> less = new Queue<>();
+            Queue<Item> equal = new Queue<>();
+            Queue<Item> greater = new Queue<>();
+            partition(items, pivot, less, equal, greater);
+            Queue<Item> sortedQueue = catenate(quickSort(less), equal);
+            sortedQueue = catenate(sortedQueue, greater);
+            return sortedQueue;
+        }
+    }
+    public static void main(String[] args) {
+        Queue<String> empires = new Queue<String>();
+        String[] empireList = {"China", "Babylon", "India", "Rome", "Cree"};
+
+        for (String item : empireList) {
+            empires.enqueue(item);
+            System.out.print(item + " ");
+        }
+        System.out.print("\n");
+        Queue<String> sortedEmpires = QuickSort.quickSort(empires);
+        for (String item : sortedEmpires) {
+            System.out.print(item + " ");
+        }
     }
 }
+/*,"Indonesia", "Khmer", "England", "Sumeria", "Macedon",
+"Aztec", "France", "Spain", "Colombia", "Scythia",
+"Carthage", "Zulu", "Congo", "Korea", "Japan", "Ethiopia"*/
